62fda345a169cf092b6a3d1363ab641d7519141d
林韋廷 <alan885544@gmaiil.com>
2023-03-02 22:49:07 +0800

    git init

---
+++ form.js
@@ -0,0 +1,44 @@
+const {createApp} = Vue;
+
+Object.keys(VeeValidateRules).forEach(rule => {
+    if (rule !== 'default') {
+      VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+  }
+});
+
+// 讀取外部的資源
+VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
+
+// Activate the locale
+VeeValidate.configure({
+  generateMessage: VeeValidateI18n.localize('zh_TW'),
+  validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
+});
+const app = createApp({
+  data(){
+    return{
+      text:'這是一段文字',
+      user:{
+        
+
+      },
+    }
+  },
+  methods:{
+    onSubmit(){
+      console.log('submit');
+    },
+    isPhone(value) {
+      const phoneNumber = /^(09)[0-9]{8}$/
+      return phoneNumber.test(value) ? true : '需要正確的電話號碼'
+    }
+  },
+  
+});
+
+
+app.component('VForm', VeeValidate.Form);
+app.component('VField', VeeValidate.Field);
+app.component('ErrorMessage', VeeValidate.ErrorMessage);
+
+app.mount('#app');
